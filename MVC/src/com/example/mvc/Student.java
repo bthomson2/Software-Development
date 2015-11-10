6e6be3581fbac1ca7275e445ff6e7aff4fb7bcfe
Brenda Thomson <thomsonclanaz@gmail.com>
2015-11-10 18:40:43 -0500

    Add MVC
    Add MVC Pattern

---
+++ MVC/src/com/example/mvc/Student.java
@@ -0,0 +1,25 @@
+package com.example.mvc;
+
+/**
+ * Created by Brenda on 11/10/2015.
+ */
+public class Student {
+    private String rollNo;
+    private String name;
+
+    public String getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(String rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ MVC/src/com/example/mvc/StudentView.java
@@ -0,0 +1,12 @@
+package com.example.mvc;
+
+/**
+ * Created by Brenda on 11/10/2015.
+ */
+public class StudentView {
+    public void printStudentDetails(String studentName, String studentRollNo){
+        System.out.println("Student: ");
+        System.out.println("Name: " + studentName);
+        System.out.println("Roll No: " + studentRollNo);
+    }
+}
